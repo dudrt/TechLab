22e768ea066f7166ac4c3787450f18faafd2f7ab
durt <89606226+dudrt@users.noreply.github.com>
2024-03-31 23:35:12 -0300

    Update index.js

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,13 +3,7 @@ import 'firebase/firestore'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-elxDRDF9L77YMt0qf55eID-x9FC7u2k",
-    authDomain: "teste-f42e9.firebaseapp.com",
-    projectId: "teste-f42e9",
-    storageBucket: "teste-f42e9.appspot.com",
-    messagingSenderId: "485401936252",
-    appId: "1:485401936252:web:d508ab3306ce84d916a4f9",
-    measurementId: "G-7MEXVYND1G"
+   
   };
   
   // Initialize Firebase
